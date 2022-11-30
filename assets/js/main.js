0106d7ac901ecab38fde531fc21cff8e40e26ea0
vovan <vovanshil95@mail.ru>
2022-11-30 03:16:58 +0300

    add dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,5 +84,68 @@ let swiper = new Swiper('.swiper', {
 
     mouseWheel: true,
     keyboard: true,
+});
 
-  });
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    
+    const scrolly = window.pageYOffset
+    
+    sections.forEach(current => {
+
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+        
+        if (scrolly > sectionTop && scrolly <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href="#' + sectionId + '"]').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href="#' + sectionId + '"]').classList.remove('active-link')
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive)
+
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+
+    if(this.scrollY >= 10) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up')
+
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-item')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => document.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if(selectedTheme) {
+
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme)
+    localStorage.setItem('selected-icon', getCurrentIcon)
+})
