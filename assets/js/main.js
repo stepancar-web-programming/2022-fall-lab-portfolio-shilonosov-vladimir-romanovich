000c75684589f115934c762820be15b12ab58dbc
vovan <vovanshil95@mail.ru>
2022-12-16 19:43:01 +0300

    change css according to bem standard split js into multiple files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,3 @@
-// MENU SHOW Y HIDDEN
-
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
-
-if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
-  });
-}
-
-if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-  });
-}
-
-const navLink = document.querySelectorAll('.nav__link');
-
-function linkAction() {
-  navMenu.classList.remove('show-menu');
-}
-
-navLink.forEach((n) => n.addEventListener('click', linkAction));
-
 const skillsContent = document.getElementsByClassName('skills__content');
 const skillsHeader = document.querySelectorAll('.skills__header');
 
@@ -34,13 +8,13 @@ function toggleSkills() {
     if (this.parentNode === skillsContent[i]) {
       if (i === openSkillsNum) {
         openSkillsNum = -1;
-        skillsContent[i].className = 'skills__content skills__close';
+        skillsContent[i].className = 'skills__content skills--closed';
       } else {
         openSkillsNum = i;
-        skillsContent[i].className = 'skills__content skills__open';
+        skillsContent[i].className = 'skills__content skills--opened';
       }
     } else {
-      skillsContent[i].className = 'skills__content skills__close';
+      skillsContent[i].className = 'skills__content skills--closed';
     }
   }
 }
@@ -56,7 +30,7 @@ const modalCloses = document.querySelectorAll('.services__modal-close');
 function openModal() {
   for (let i = 0; i < modalBtns.length; i += 1) {
     if (this === modalBtns[i]) {
-      modalViews[i].classList.add('active-modal');
+      modalViews[i].classList.add('modal--active');
     }
   }
 }
@@ -68,7 +42,7 @@ modalBtns.forEach((modalBtn) => {
 modalCloses.forEach((modalClose) => {
   modalClose.addEventListener('click', () => {
     modalViews.forEach((modalView) => {
-      modalView.classList.remove('active-modal');
+      modalView.classList.remove('modal--active');
     });
   });
 });
@@ -91,61 +65,6 @@ const swiper = new Swiper('.swiper', {
 
   mouseWheel: true,
   keyboard: true
-});
-
-const sections = document.querySelectorAll('section[id]');
-
-function scrollActive() {
-  const scrolly = window.pageYOffset;
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    const sectionId = current.getAttribute('id');
-
-    if (scrolly > sectionTop && scrolly <= sectionTop + sectionHeight) {
-      document.querySelector(`.nav__menu a[href="#${sectionId}"]`).classList.add('active-link');
-    } else {
-      document.querySelector(`.nav__menu a[href="#${sectionId}"]`).classList.remove('active-link');
-    }
-  });
-}
-window.addEventListener('scroll', scrollActive);
-
-function scrollHeader() {
-  const nav = document.getElementById('header');
-  if (this.scrollY >= 10) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
-}
-window.addEventListener('scroll', scrollHeader);
-
-function scrollUp() {
-  const scrollUpEl = document.getElementById('scroll-up');
-
-  if (this.scrollY >= 560) scrollUpEl.classList.add('show-scroll'); else scrollUpEl.classList.remove('show-scroll');
-}
-window.addEventListener('scroll', scrollUp);
-
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'uil-sun';
-
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-item');
-
-const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
-const getCurrentIcon = () => (document.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun');
-
-if (selectedTheme) {
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
-}
-
-themeButton.addEventListener('click', () => {
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-
-  localStorage.setItem('selected-theme', getCurrentTheme);
-  localStorage.setItem('selected-icon', getCurrentIcon);
 });
 
 function redirect() {
